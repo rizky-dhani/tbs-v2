1096c56269d6d42489820c1cdb627e9cd8cd34c7
Rizky Dhani <rizkydhni03@gmail.com>
2025-09-25 09:57:25 +0700

    feat(filament): add navigation group and permissions to UserResource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -20,6 +20,12 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationGroup = 'User Settings';
+    protected static ?int $navigationSort = 1;
+    public static function canViewAny(): bool
+    {
+        return auth()->user()->hasRole(['Superadmin']);
+    }
 
     public static function form(Form $form): Form
     {
